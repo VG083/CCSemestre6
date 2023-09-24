9d76dd6e62392d4c31ae06c85ddf0aedc1e3d89a
VG083 <83486329+VG083@users.noreply.github.com>
2023-09-24 15:23:50 -0300

    Add files via upload

--- ComputacaoGrafica/my_gl_framework/mygl.cpp
+++ ComputacaoGrafica/my_gl_framework/mygl.cpp
@@ -5,67 +5,93 @@
 
 using namespace std;
 
-//-----------------------------------------------------------------------------
-void MyGlDraw(void)
-{
-    //*************************************************************************
-    // Chame aqui as funções do mygl.h
-    //*************************************************************************
-    // Exemplo de uso da função DrawLine para desenhar uma linha vermelha de (10, 10) a (100, 100):
-    DrawLine(10, 10, 255, 0, 0, 255,
-            500, 500, 255, 0, 0, 255);
+void MyGlDraw(void) {
+    DrawTriangle(0, 0, 0, 255, 0, 255,
+                 256, 256, 0, 0, 255, 255,
+                 128, 0, 255, 0, 0, 255);
+    DrawTriangle(128, 0, 0, 255, 0, 255,
+                 256, 256, 0, 0, 255, 255,
+                 256, 0, 255, 0, 0, 255);
+    DrawTriangle(256, 0, 0, 255, 0, 255,
+                 256, 256, 0, 0, 255, 255,
+                 384, 0, 255, 0, 0, 255);
+    DrawTriangle(384, 0, 0, 255, 0, 255,
+                 256, 256, 0, 0, 255, 255,
+                 512, 0, 255, 0, 0, 255);
+    DrawTriangle(512, 0, 0, 255, 0, 255,
+                 256, 256, 0, 0, 255, 255,
+                 512, 128, 255, 0, 0, 255);
+    DrawTriangle(512, 128, 0, 255, 0, 255,
+                 256, 256, 0, 0, 255, 255,
+                 512, 256, 255, 0, 0, 255);
+    DrawTriangle(512, 256, 0, 255, 0, 255,
+                 256, 256, 0, 0, 255, 255,
+                 512, 384, 255, 0, 0, 255);
+    DrawTriangle(512, 384, 0, 255, 0, 255,
+                 256, 256, 0, 0, 255, 255,
+                 512, 512, 255, 0, 0, 255);
+    DrawTriangle(512, 512, 0, 255, 0, 255,
+                 256, 256, 0, 0, 255, 255,
+                 384, 512, 255, 0, 0, 255);
+    DrawTriangle(384, 512, 0, 255, 0, 255,
+                 256, 256, 0, 0, 255, 255,
+                 256, 512, 255, 0, 0, 255);
+    DrawTriangle(256, 512, 0, 255, 0, 255,
+                 256, 256, 0, 0, 255, 255,
+                 128, 512, 255, 0, 0, 255);
+    DrawTriangle(128, 512, 0, 255, 0, 255,
+                 256, 256, 0, 0, 255, 255,
+                 0, 512, 255, 0, 0, 255);
+    DrawTriangle(0, 512, 0, 255, 0, 255,
+                 256, 256, 0, 0, 255, 255,
+                 0, 384, 255, 0, 0, 255);
+    DrawTriangle(0, 384, 0, 255, 0, 255,
+                 256, 256, 0, 0, 255, 255,
+                 0, 255, 255, 0, 0, 255);
+    DrawTriangle(0, 256, 0, 255, 0, 255,
+                 256, 256, 0, 0, 255, 255,
+                 0, 128, 0, 255, 0, 255);
 }
 
-void PutPixel(int x, int y, int R, int G, int B, int A)
-{
-    // Calcula o índice do pixel no framebuffer com base nas coordenadas (x, y):
+void PutPixel(int x, int y, int R, int G, int B, int A) {
     int index = 4 * (x + y * IMAGE_WIDTH);
-
-    // Define as componentes RGBA do pixel no framebuffer:
-    FBptr[index + 0] = R; // componente R
-    FBptr[index + 1] = G; // componente G
-    FBptr[index + 2] = B; // componente B
-    FBptr[index + 3] = A; // componente A
+    FBptr[index + 0] = R;
+    FBptr[index + 1] = G;
+    FBptr[index + 2] = B;
+    FBptr[index + 3] = A;
 }
 
 void DrawLine(int x0, int y0, int R0, int G0, int B0, int A0,
-              int x1, int y1, int R1, int G1, int B1, int A1)
-{
-    // Calcula as diferenças entre as coordenadas x e y dos pontos iniciais e finais
-    int dx = abs(x1 - x0);  // Diferença no eixo x
-    int dy = abs(y1 - y0);  // Diferença no eixo y
-    // Determina a direção da linha (1 ou -1) ao longo dos eixos x e y
-    int sx = (x0 < x1) ? 1 : -1;  // Direção no eixo x
-    int sy = (y0 < y1) ? 1 : -1;  // Direção no eixo y
-    // Calcula o erro inicial
-    int err = dx - dy;
-    // Loop principal para desenhar a linha pixel a pixel
-    while (true)
-    {
-        // Calcula o fator de interpolação (t) entre os pontos iniciais e finais
-        float t = static_cast<float>(dx + dy - err) / (2 * dx + 2 * dy);
-        // Interpola as componentes R, G, B e A da cor entre os pontos iniciais e finais
-        int R = static_cast<int>(R0 + t * (R1 - R0));  // Interpolação da componente R
-        int G = static_cast<int>(G0 + t * (G1 - G0));  // Interpolação da componente G
-        int B = static_cast<int>(B0 + t * (B1 - B0));  // Interpolação da componente B
-        int A = static_cast<int>(A0 + t * (A1 - A0));  // Interpolação da componente A
-        // Chama a função PutPixel para definir a cor do pixel atual
-        PutPixel(x0, y0, R, G, B, A);
+              int x1, int y1, int R1, int G1, int B1, int A1) {
+    int dx = abs(x1 - x0), dy = abs(y1 - y0);
+    int sx = (x0 < x1) ? 1 : -1, sy = (y0 < y1) ? 1 : -1;
+    int err = dx - dy, e2;
+    float t;
 
-        // Verifica se chegamos ao ponto final da linha
-        if (x0 == x1 && y0 == y1)
-            break;
-        // Calcula o erro para o próximo pixel e atualiza as coordenadas x e y
-        int e2 = 2 * err;
-        if (e2 > -dy)
-        {
+    while (x0 != x1 || y0 != y1) {
+        t = static_cast<float>(x0 - x1) / dx;
+        PutPixel(
+            x0, y0, R0 + static_cast<int>(t * (R1 - R0)),
+            G0 + static_cast<int>(t * (G1 - G0)),
+            B0 + static_cast<int>(t * (B1 - B0)),
+            A0 + static_cast<int>(t * (A1 - A0))
+        );
+        e2 = 2 * err;
+        if (e2 > -dy) {
             err -= dy;
-            x0 += sx;  // Atualiza a coordenada x
+            x0 += sx;
         }
-        if (e2 < dx)
-        {
+        if (e2 < dx) {
             err += dx;
-            y0 += sy;  // Atualiza a coordenada y
+            y0 += sy;
         }
     }
+}
+
+void DrawTriangle(int x0, int y0, int R0, int G0, int B0, int A0,
+                  int x1, int y1, int R1, int G1, int B1, int A1,
+                  int x2, int y2, int R2, int G2, int B2, int A2) {
+    DrawLine(x0, y0, R0, G0, B0, A0, x1, y1, R1, G1, B1, A1);
+    DrawLine(x1, y1, R1, G1, B1, A1, x2, y2, R2, G2, B2, A2);
+    DrawLine(x2, y2, R2, G2, B2, A2, x0, y0, R0, G0, B0, A0);
 }
--- ComputacaoGrafica/my_gl_framework/mygl.h
+++ ComputacaoGrafica/my_gl_framework/mygl.h
@@ -4,12 +4,17 @@
 
 #include "definitions.h"
 
-//-----------------------------------------------------------------------------
 void MyGlDraw(void);
 
-//*****************************************************************************
-// Defina aqui as suas funções gráficas
-//*****************************************************************************
-
-void PutPixel(int x, int y, int R, int G, int B, int A);
-void DrawLine(int x0, int y0, int R0, int G0, int B0, int A0, int x1, int y1, int R1, int G1, int B1, int A1);
+void PutPixel(
+    int x, int y, int R, int G, int B, int A
+);
+void DrawLine(
+    int x0, int y0, int R0, int G0, int B0, int A0, 
+    int x1, int y1, int R1, int G1, int B1, int A1
+);
+void DrawTriangle(
+    int x0, int y0, int R0, int G0, int B0, int A0, 
+    int x1, int y1, int R1, int G1, int B1, int A1, 
+    int x2, int y2, int R2, int G2, int B2, int A2
+);
